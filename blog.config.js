b64326c3ca2f22e1aab3d5efc3e0a0ece731f2ba
zhangdi <1258956799@qq.com>
2023-04-15 22:21:22 +0800

    修改title

--- blog.config.js
+++ blog.config.js
@@ -2,12 +2,12 @@
  * @Author: zhangdi 1258956799@qq.com
  * @Date: 2023-04-15 21:50:32
  * @LastEditors: zhangdi 1258956799@qq.com
- * @LastEditTime: 2023-04-15 21:51:55
+ * @LastEditTime: 2023-04-15 22:16:37
  * @FilePath: /nobelium/blog.config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 const BLOG = {
-  title: '花开富贵',
+  title: '富贵',
   author: '富贵',
   email: 'z1258956799@gmail.com',
   link: 'https://nobelium.vercel.app',
